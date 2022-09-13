d5cfdfd2cf70b4551e85a3f85812d284dac41686
William Oliveira <48337511+willraoli@users.noreply.github.com>
2022-09-13 20:53:50 -0300

    feat: add validation & better layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -57,8 +57,11 @@ const Home: NextPage<HomeProps> = ({ tweets }) => {
           href='/twitter.svg'
         />
       </Head>
-      <main className='mx-auto min-h-screen max-w-xl border-x-2 border-neutral-200 p-8'>
-        <form onSubmit={handleSubmit}>
+      <main className='mx-auto min-h-screen max-w-xl border-x border-neutral-200'>
+        <form
+          onSubmit={handleSubmit}
+          className='p-4'
+        >
           <div className='mb-2 flex items-center gap-2'>
             {session && (
               <Image
@@ -73,12 +76,15 @@ const Home: NextPage<HomeProps> = ({ tweets }) => {
               value={text}
               className='h-20 w-full resize-none border border-neutral-300 p-2 text-lg'
               placeholder='O que está acontecendo?'
+              maxLength={280}
+              minLength={1}
             />
           </div>
           <div className='flex justify-end'>
             <button
               type='submit'
-              className='rounded-3xl bg-blue-500 py-2 px-5 text-right font-semibold text-white'
+              className='rounded-3xl bg-blue-500 py-2 px-5 text-right font-semibold text-white transition-all hover:bg-blue-600 active:bg-blue-500 disabled:opacity-30 disabled:hover:bg-blue-500'
+              disabled={text.length === 0}
             >
               Tweetar
             </button>
@@ -88,27 +94,41 @@ const Home: NextPage<HomeProps> = ({ tweets }) => {
           {tweetList?.map(tweet => (
             <li
               key={tweet.id}
-              className='flex h-36 items-start justify-between gap-2 border-y pt-4'
+              className='h-min-36 flex flex-col border-t p-4'
             >
-              <div className='flex gap-4'>
+              <div className='flex h-full place-items-start gap-2'>
                 {tweet.author.image && (
-                  <Image
-                    src={tweet.author.image}
-                    height='48px'
-                    width='48px'
-                    className='rounded-full'
-                  />
+                  <div className='w-[48px]'>
+                    <Image
+                      src={tweet.author.image}
+                      height='48px'
+                      width='48px'
+                      layout='fixed'
+                      className='rounded-full'
+                    />
+                  </div>
                 )}
                 <div className='flex flex-col'>
-                  <a className='text-sm font-semibold'>@{tweet.author.name}</a>
-                  <span>{tweet.content}</span>
+                  <a className='text-sm font-semibold'>
+                    @{tweet.author.name?.split(' ')[0]}
+                  </a>
+                  <p className='text-sm'>{tweet.content}</p>
                 </div>
               </div>
-              <div className='flex items-center gap-2'>
-                <span className='text-sm text-neutral-400'>
-                  {tweet.likes} likes
-                </span>
-                <button onClick={() => likeTweet({ id: tweet.id })}>❤️</button>
+              <div className='flex justify-end'>
+                <button
+                  onClick={() => likeTweet({ id: tweet.id })}
+                  className='group'
+                  disabled={text.length === 0}
+                >
+                  <span className='mr-2 rounded-full p-1 group-hover:bg-rose-200'>
+                    ❤️
+                  </span>
+                  <span className='text-sm text-neutral-400 group-hover:text-rose-500'>
+                    {tweet.likes} like
+                    {(tweet.likes === 0 && 's') || (tweet.likes > 1 && 's')}
+                  </span>
+                </button>
               </div>
             </li>
           ))}
